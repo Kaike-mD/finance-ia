0bde2dc5c45e550acc1f88c80aa9e016aab8e1b0
kaike.fernandes <kaike.fernandes@meuinc.com.br>
2024-11-07 13:12:31 -0300

    feat: update transaction page add conection with db

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,16 @@
-const TransactionsPage = () => {
-  return <h1>Transaction page</h1>;
+import { db } from "../_lib/prisma";
+
+const TransactionsPage = async () => {
+  // acessar as transações do banco de dados
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <div>
+      {transactions.map((transaction) => (
+        <div key={transaction.id}>{transaction.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default TransactionsPage;
